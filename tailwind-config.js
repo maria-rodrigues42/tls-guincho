77eec96d6ff7b6f325d6ab74423bce08c012790c
maria <mariarodriggues42@gmail.com>
2026-07-21 18:18:54 -0300

    refactor: extract inline CSS and scripts to separate files

---
+++ tailwind-config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        fundo: '#F7F8F0',
+        marca: { DEFAULT: '#4A7A9D', clara: '#9CD5FF', escura: '#1c2830' },
+        zap: { DEFAULT: '#25D366', dark: '#08210F' }
+      },
+      fontFamily: {
+        bricolage: ['"Bricolage Grotesque"', 'sans-serif'],
+        noto: ['"Noto Sans"', 'sans-serif'],
+        mono: ['"IBM Plex Mono"', 'monospace'],
+        zalando: ['Archivo', 'sans-serif'],
+      }
+    }
+  }
+};
